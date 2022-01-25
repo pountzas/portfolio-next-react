a58530a4eec835fd0b29014e8c8ff090813ac488
pountzas <nikos@pountzas.gr>
2022-01-26 00:03:58 +0200

    add icons to use on Header

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,8 @@
+import { IoHome, IoIosContact } from 'react-icons/io';
+import { FcAboutUs } from 'react-icons/fc';
+import { GiSkills } from 'react-icons/gi';
+import { RiGitRepositoryLine } from 'react-icons/ri';
+
 function Header() {
   return (
     <div className='flex shadow-sm border-b bg-white sticky top-0 z-50 '>
