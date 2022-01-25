9296edb0d225027564d94e95ab74984a60cef31f
pountzas <nikos@pountzas.gr>
2022-01-25 23:51:11 +0200

    add basic header comp

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className='flex shadow-sm border-b bg-white sticky top-0 z-50 '>
+      header
+      <div>right</div>
+      <div>left</div>
+    </div>
+  );
+}
+
+export default Header;
