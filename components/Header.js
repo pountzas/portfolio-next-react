8917483c764395aa63fb41ec970ba3c76a6d7a81
pountzas <nikos@pountzas.gr>
2022-01-27 01:20:59 +0200

    remove about, change some icons and add all icons to header

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,32 @@
-import { IoHome, IoIosContact } from 'react-icons/io';
-import { FcAboutUs } from 'react-icons/fc';
+import { IoIosContact } from 'react-icons/io';
 import { GiSkills } from 'react-icons/gi';
 import { RiGitRepositoryLine } from 'react-icons/ri';
+import { FaHome } from 'react-icons/fa';
 
 function Header() {
   return (
     <div className='flex shadow-sm border-b bg-white sticky top-0 z-50 '>
       {/* left section */}
-      <div>left section</div>
+      <h1>Nikos Pountzas</h1>
 
       {/* right section */}
-      <ul className='flex justify-between'>
-        <li className='px-1'>Home</li>
-        <li className='px-1'>About</li>
-        <li className='px-1'>Projects</li>
-        <li className='px-1'>Skills</li>
-        <li className='px-1'>Contact</li>
+      <ul className='flex justify-between text-gray-500'>
+        <li className='hidden md:block px-1 text-lg uppercase font-semibold'>
+          Home
+        </li>
+        <FaHome className='text-2xl md:hidden' />
+        <li className='hidden md:block px-1 text-lg uppercase font-semibold'>
+          Projects
+        </li>
+        <RiGitRepositoryLine className='text-2xl md:hidden' />
+        <li className='hidden md:block px-1 text-lg uppercase font-semibold'>
+          Skills
+        </li>
+        <GiSkills className='text-2xl md:hidden' />
+        <li className='hidden md:block px-1 text-lg uppercase font-semibold'>
+          Contact
+        </li>
+        <IoIosContact className='text-2xl md:hidden' />
       </ul>
     </div>
   );
