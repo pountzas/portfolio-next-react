f0cee40bf081e0872ef0e8ff96cb75eafef8f6dc
pountzas <nikos@pountzas.gr>
2022-01-27 02:46:53 +0200

    use next router on pages

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,11 @@ import { GiSkills } from 'react-icons/gi';
 import { RiGitRepositoryLine } from 'react-icons/ri';
 import { FaHome } from 'react-icons/fa';
 
+import { useRouter } from 'next/router';
+
 function Header() {
+  const router = useRouter();
+
   return (
     <div className='flex shadow-sm border-b bg-white sticky top-0 z-50 '>
       {/* left section */}
@@ -11,22 +15,34 @@ function Header() {
 
       {/* right section */}
       <ul className='flex justify-between text-gray-500'>
-        <li className='hidden md:block px-1 text-lg uppercase font-semibold'>
+        <li
+          onClick={() => router.push('/')}
+          className='hidden md:block px-1 text-lg uppercase font-semibold'
+        >
           Home
+          <FaHome className='text-2xl md:hidden' />
         </li>
-        <FaHome className='text-2xl md:hidden' />
-        <li className='hidden md:block px-1 text-lg uppercase font-semibold'>
+        <li
+          onClick={() => router.push('/Projects')}
+          className='hidden md:block px-1 text-lg uppercase font-semibold'
+        >
           Projects
+          <RiGitRepositoryLine className='text-2xl md:hidden' />
         </li>
-        <RiGitRepositoryLine className='text-2xl md:hidden' />
-        <li className='hidden md:block px-1 text-lg uppercase font-semibold'>
+        <li
+          onClick={() => router.push('/Skills')}
+          className='hidden md:block px-1 text-lg uppercase font-semibold'
+        >
           Skills
+          <GiSkills className='text-2xl md:hidden' />
         </li>
-        <GiSkills className='text-2xl md:hidden' />
-        <li className='hidden md:block px-1 text-lg uppercase font-semibold'>
+        <li
+          onClick={() => router.push('/Contact')}
+          className='hidden md:block px-1 text-lg uppercase font-semibold'
+        >
           Contact
+          <IoIosContact className='text-2xl md:hidden' />
         </li>
-        <IoIosContact className='text-2xl md:hidden' />
       </ul>
     </div>
   );
