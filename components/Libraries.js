dfaedc30ed200e5fb57a267248140f73b2e76a09
pountzas <nikos@pountzas.gr>
2022-01-28 23:56:52 +0200

    add Libraries comp

---
+++ components/Libraries.js
@@ -0,0 +1,17 @@
+import { FaReact } from 'react-icons/fa';
+import { SiRedux } from 'react-icons/si';
+
+const Libraries = [
+  {
+    id: 1,
+    name: 'React',
+    icon: <FaReact />,
+  },
+  {
+    id: 2,
+    name: 'Redux',
+    icon: <SiRedux />,
+  },
+];
+
+export default Libraries;
