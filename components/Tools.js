40e6409eb5917539219153d61041a3b55b65fc77
pountzas <nikos@pountzas.gr>
2022-01-28 23:57:07 +0200

    add Tools comp

---
+++ components/Tools.js
@@ -0,0 +1,39 @@
+import { SiVisualstudiocode } from './SiVisualstudiocode';
+import { FaGit } from 'react-icons/fa';
+import { AiFillGithub } from 'react-icons/ai';
+import { SiAdobephotoshop } from 'SiAdobephotoshop';
+import { SiAdobeillustrator } from './SiAdobeillustrator';
+import { SiCinema4D } from './SiCinema4D';
+
+const Tools = [
+  {
+    id: 1,
+    name: 'Visual Studio Code',
+    icon: <SiVisualstudiocode />,
+  },
+  {
+    id: 2,
+    name: 'Git',
+    icon: <FaGit />,
+  },
+  {
+    id: 3,
+    name: 'Github',
+    icon: <AiFillGithub />,
+  },
+  {
+    id: 4,
+    name: 'Photoshop',
+    icon: <SiAdobephotoshop />,
+  },
+  {
+    id: 5,
+    name: 'Illustrator',
+    icon: <SiAdobeillustrator />,
+  },
+  {
+    id: 6,
+    name: 'Cinema 4D',
+    icon: <SiCinema4D />,
+  },
+];
