952479b011de55fbcd4a2da8784e3fac3c2e6d7d
pountzas <nikos@pountzas.gr>
2022-01-28 23:55:56 +0200

    add Frameworks comp

---
+++ components/Frameworks.js
@@ -0,0 +1,27 @@
+import { SiRubyonrails, SiNextdotjs, SiTailwindcss } from './SiRubyonrails';
+import { DiBootstrap } from './DiBootstrap';
+
+const Frameworks = [
+  {
+    id: 1,
+    name: 'Ruby on Rails',
+    icon: <SiRubyonrails />,
+  },
+  {
+    id: 2,
+    name: 'Next.js',
+    icon: <SiNextdotjs />,
+  },
+  {
+    id: 3,
+    name: 'Tailwind CSS',
+    icon: <SiTailwindcss />,
+  },
+  {
+    id: 4,
+    name: 'Bootstrap',
+    icon: <DiBootstrap />,
+  },
+];
+
+export default Frameworks;
