1e189470d47f32db4098122d8d3fe6741e9f01fa
pountzas <nikos@pountzas.gr>
2022-01-28 22:13:17 +0200

    add Languages comp

---
+++ components/Languages.js
@@ -0,0 +1,29 @@
+import { AiFillHtml5 } from 'react-icons/ai';
+import { SiCss3 } from 'react-icons/si';
+import { DiRubyRough } from 'react-icons/di';
+import { SiJavascript } from 'react-icons/si';
+
+const Languages = [
+  {
+    id: 1,
+    name: 'HTML5',
+    icon: <AiFillHtml5 />,
+  },
+  {
+    id: 2,
+    name: 'CSS3',
+    icon: <SiCss3 />,
+  },
+  {
+    id: 3,
+    name: 'Ruby',
+    icon: <DiRubyRough />,
+  },
+  {
+    id: 4,
+    name: 'JavaScript',
+    icon: <SiJavascript />,
+  },
+];
+
+export default Languages;
