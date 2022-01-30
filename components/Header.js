53f44f2e0b7861f559ee03f7f50463177e76f1f0
pountzas <nikos@pountzas.gr>
2022-01-31 00:23:08 +0200

    add media query styles on header

--- components/Header.js
+++ components/Header.js
@@ -12,10 +12,12 @@ function Header() {
     <div className='py-3 shadow-sm border-b bg-gray-800 sticky top-0 z-50 '>
       {/* left section */}
       <div className='flex justify-between md:max-w-3xl xl:max-w-6xl mx-auto '>
-        <h1 className='px-3 text-gray-100 font-semibold'>Nikos Pountzas</h1>
+        <h1 className='px-3 md:px-0 text-gray-100 font-semibold'>
+          Nikos Pountzas
+        </h1>
 
         {/* right section */}
-        <div className='flex space-x-8 px-3 text-gray-400'>
+        <div className='flex space-x-8 px-3 md:px-0 text-gray-400'>
           <div onClick={() => router.push('/')}>
             <div className='hidden md:block px-1 text-lg uppercase font-semibold'>
               Home
@@ -35,7 +37,7 @@ function Header() {
             <GiSkills className='text-2xl md:hidden' />
           </div>
           <div onClick={() => router.push('/Contact')}>
-            <div className='hidden md:block px-1 text-lg uppercase font-semibold'>
+            <div className='hidden md:block  text-lg uppercase font-semibold'>
               Contact
             </div>
             <IoIosContact className='text-2xl md:hidden' />
