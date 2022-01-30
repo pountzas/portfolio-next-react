c4e2042166e467cad2583ebe5643c9a90729bdd3
pountzas <nikos@pountzas.gr>
2022-01-31 00:42:32 +0200

    add hover effect on skill tabs

--- pages/Skills.js
+++ pages/Skills.js
@@ -6,7 +6,7 @@ import Tools from '../components/Tools';
 
 function Skills() {
   return (
-    <div className='bg-gray-900 min-h-screen '>
+    <div className='bg-gray-900 min-h-screen'>
       <Header />
       <h1 className='text-gray-200 text-center font-semibold text-2xl pt-6 sm:pt-12'>
         My Skillset
@@ -21,7 +21,7 @@ function Skills() {
           <div className='grid grid-cols-2 gap-4 place-items-center '>
             {Languages.map((language) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-red-700 ease-in-out duration-400'
+                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:bg-gray-800 ease-in-out duration-400'
                 key={language.id}
               >
                 <div className='text-3xl md:text-5xl'>
@@ -42,7 +42,7 @@ function Skills() {
           <div className='grid grid-cols-2 gap-4 place-items-center'>
             {Libraries.map((library) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-red-700 ease-in-out duration-400'
+                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:bg-gray-800 ease-in-out duration-400 shadow-white'
                 key={library.id}
               >
                 <div className='text-3xl md:text-5xl'>
@@ -57,7 +57,7 @@ function Skills() {
           <div className='pt-2 grid grid-cols-2 gap-4 place-items-center'>
             {Frameworks.map((framework) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-red-700 ease-in-out duration-400'
+                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:bg-gray-800 ease-in-out duration-400'
                 key={framework.id}
               >
                 <div className='text-3xl md:text-5xl'>
@@ -78,7 +78,7 @@ function Skills() {
           <div className='grid grid-cols-2 gap-4 place-items-center'>
             {Tools.map((tool) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-red-700 ease-in-out duration-400'
+                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:bg-gray-800 ease-in-out duration-400'
                 key={tool.id}
               >
                 <div className='text-3xl md:text-5xl'>
