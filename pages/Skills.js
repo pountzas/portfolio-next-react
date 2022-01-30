ef0c6153756c875179bd56f0c17db3a96a23856c
pountzas <nikos@pountzas.gr>
2022-01-31 00:24:12 +0200

    add media query styles and proper alignments for skills

--- pages/Skills.js
+++ pages/Skills.js
@@ -8,15 +8,20 @@ function Skills() {
   return (
     <div className='bg-gray-900 min-h-screen '>
       <Header />
-      <h1 className='text-gray-200 text-center font-semibold text-2xl p-3'>
+      <h1 className='text-gray-200 text-center font-semibold text-2xl pt-6 sm:pt-12'>
         My Skillset
       </h1>
-      <div className='sm:flex justify-between md:max-w-3xl xl:max-w-7xl mx-auto'>
-        <section className='container'>
-          <div className='grid grid-cols-2 gap-2 place-items-center '>
+      <div className='sm:grid grid-cols-3 gap-4 md:max-w-4xl lg:max-w-7xl px-3 mx-auto'>
+        {/* <div className='sm:flex justify-between md:max-w-4xl mx-auto'> */}
+
+        <section className='container flex flex-col items-center'>
+          <h2 className='text-gray-200 text-center py-4 sm:py-8'>
+            Programing Languages
+          </h2>
+          <div className='grid grid-cols-2 gap-4 place-items-center '>
             {Languages.map((language) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500'
+                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-red-700 ease-in-out duration-400'
                 key={language.id}
               >
                 <div className='text-3xl md:text-5xl'>
@@ -30,11 +35,14 @@ function Skills() {
           </div>
         </section>
 
-        <section className='container'>
-          <div className='grid grid-cols-2 gap-2 place-items-center '>
+        <section className='container flex flex-col items-center'>
+          <h2 className='text-gray-200 text-center py-4 sm:py-8'>
+            Libraries & Frameworks
+          </h2>
+          <div className='grid grid-cols-2 gap-4 place-items-center'>
             {Libraries.map((library) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500'
+                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-red-700 ease-in-out duration-400'
                 key={library.id}
               >
                 <div className='text-3xl md:text-5xl'>
@@ -46,10 +54,10 @@ function Skills() {
               </div>
             ))}
           </div>
-          <div className='pt-2 grid grid-cols-2 gap-2 place-items-center '>
+          <div className='pt-2 grid grid-cols-2 gap-4 place-items-center'>
             {Frameworks.map((framework) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500'
+                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-red-700 ease-in-out duration-400'
                 key={framework.id}
               >
                 <div className='text-3xl md:text-5xl'>
@@ -63,11 +71,14 @@ function Skills() {
           </div>
         </section>
 
-        <section className='container'>
-          <div className='grid grid-cols-2 gap-2 place-items-center '>
+        <section className='container flex flex-col items-center'>
+          <h2 className='text-gray-200 text-center py-4 sm:py-8'>
+            Tools and Software
+          </h2>
+          <div className='grid grid-cols-2 gap-4 place-items-center'>
             {Tools.map((tool) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500'
+                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-red-700 ease-in-out duration-400'
                 key={tool.id}
               >
                 <div className='text-3xl md:text-5xl'>
