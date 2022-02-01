6c7510ac5250446ca5c474d31551b0dbd87f366f
pountzas <nikos@pountzas.gr>
2022-02-01 21:59:36 +0200

    add more tools for skillset

--- components/Tools.js
+++ components/Tools.js
@@ -3,6 +3,9 @@ import {
   SiAdobephotoshop,
   SiAdobeillustrator,
   SiCinema4D,
+  SiFirebase,
+  SiNetlify,
+  SiHeroku,
 } from 'react-icons/si';
 import { FaGit } from 'react-icons/fa';
 import { AiFillGithub } from 'react-icons/ai';
@@ -37,6 +40,21 @@ const Tools = [
     id: 6,
     name: 'Cinema 4D',
     icon: <SiCinema4D />,
+  },
+  {
+    id: 7,
+    name: 'Firebase',
+    icon: <SiFirebase />,
+  },
+  {
+    id: 8,
+    name: 'Netlify',
+    icon: <SiNetlify />,
+  },
+  {
+    id: 9,
+    name: 'Heroku',
+    icon: <SiHeroku />,
   },
 ];
 
