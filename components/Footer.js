f7b8c030378783afe9740ad8111bdf2a525322c1
pountzas <nikos@pountzas.gr>
2022-02-04 23:58:00 +0200

    add socials to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Socials from './Socials';
+import Link from 'next/link';
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>footer</h1>
+      <div className='flex items-center justify-center'>
+        {Socials.map((social) => (
+          <div href={social.url} key={social.id} className=''>
+            <a className='text-gray-800' href={social.url}>
+              {social.icon}
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
