0e819dd2d236572a4695a7b03667c793570041b7
pountzas <nikos@pountzas.gr>
2022-02-05 23:23:42 +0200

    add custom icon FaRecoil
    Description the custom icon was add localy on the node-modules/react-icons/fa. Declaired the new const and create on index files

--- components/Libraries.js
+++ components/Libraries.js
@@ -1,5 +1,6 @@
-import { FaReact } from 'react-icons/fa';
+import { FaReact, FaRecoil } from 'react-icons/fa';
 import { SiRedux } from 'react-icons/si';
+import { recoil } from './customIcons/recoil';
 
 const Libraries = [
   {
@@ -12,6 +13,15 @@ const Libraries = [
     name: 'Redux',
     icon: <SiRedux />,
   },
+  {
+    id: 3,
+    name: 'Recoil',
+    icon: <FaRecoil />,
+  },
+  // {
+  //   id: 4,
+  //   name: 'React Native',
+  // },
 ];
 
 export default Libraries;
