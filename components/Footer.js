ab17e91b5468949af628b73a1e741147a90399cb
pountzas <nikos@pountzas.gr>
2022-02-06 19:36:10 +0200

    add left and midle section of footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,16 +3,17 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div>
-      <h1>footer</h1>
-      <div className='flex items-center justify-center'>
-        {Socials.map((social) => (
-          <div key={social.id}>
-            <Link href={social.path} passHref>
-              <a target='_blank'>{social.icon}</a>
-            </Link>
-          </div>
-        ))}
+        <div>Designed and Developed by Pountzas Nikos</div>
+        <div>Copyright © 2022 NP</div>
+        <div className='flex items-center justify-center'>
+          {Socials.map((social) => (
+            <div key={social.id}>
+              <Link href={social.path} passHref>
+                <a target='_blank'>{social.icon}</a>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
