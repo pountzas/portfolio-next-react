e892e73a8bb0200b6e42d8e40e49714b115d900e
pountzas <nikos@pountzas.gr>
2022-02-06 20:15:16 +0200

    readjust footer max-w

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 function Footer() {
   return (
     <div className='bg-gray-900 bottom-0 min-w-full fixed border-t'>
-      <div className='md:max-w-3xl xl:max-w-6xl mx-auto text-gray-200 flex justify-between items-center'>
+      <div className='md:max-w-4xl xl:max-w-6xl mx-auto text-gray-200 flex justify-between items-center'>
         <div>Designed and Developed by Pountzas Nikos</div>
         <div>Copyright © 2022 NP</div>
         <div className='flex items-center justify-center space-x-2 py-2'>
