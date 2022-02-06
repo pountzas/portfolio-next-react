0d7ed532045080bbfd81cb2c83661c2d7fe37b7c
pountzas <nikos@pountzas.gr>
2022-02-06 19:40:45 +0200

    add styles and transitions on footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,13 +3,17 @@ import Link from 'next/link';
 
 function Footer() {
   return (
+    <div className='bg-gray-900 bottom-0 min-w-full fixed'>
+      <div className='md:max-w-3xl xl:max-w-6xl mx-auto text-gray-200 flex justify-between items-center'>
         <div>Designed and Developed by Pountzas Nikos</div>
         <div>Copyright © 2022 NP</div>
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center space-x-2 py-2'>
           {Socials.map((social) => (
             <div key={social.id}>
               <Link href={social.path} passHref>
-                <a target='_blank'>{social.icon}</a>
+                <a className='text-2xl' target='_blank'>
+                  {social.icon}
+                </a>
               </Link>
             </div>
           ))}
