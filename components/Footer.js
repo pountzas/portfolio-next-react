c72957448ac0e059813e691e3232a4b23e3c0f9d
pountzas <nikos@pountzas.gr>
2022-02-06 19:54:09 +0200

    add top border to footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className='bg-gray-900 bottom-0 min-w-full fixed'>
+    <div className='bg-gray-900 bottom-0 min-w-full fixed border-t'>
       <div className='md:max-w-3xl xl:max-w-6xl mx-auto text-gray-200 flex justify-between items-center'>
         <div>Designed and Developed by Pountzas Nikos</div>
         <div>Copyright © 2022 NP</div>
