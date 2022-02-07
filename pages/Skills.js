837357475546fec14a2003986eb2070ab0eb5bc2
pountzas <nikos@pountzas.gr>
2022-02-07 21:45:59 +0200

    add shadow and border color to skill boxes, add color to titles

--- pages/Skills.js
+++ pages/Skills.js
@@ -12,27 +12,27 @@ function Skills() {
   return (
     <div className='bg-gray-900 min-h-screen'>
       <Header />
-      <h1 className='text-gray-200 text-center font-semibold text-2xl pt-6 sm:pt-12'>
+      <h1 className='text-blue-200 text-center font-semibold text-2xl pt-6 sm:pt-12'>
         My Skillset
       </h1>
       <div className='sm:grid grid-cols-3 gap-4 md:max-w-4xl lg:max-w-7xl px-3 mx-auto'>
         {/* <div className='sm:flex justify-between md:max-w-4xl mx-auto'> */}
 
         <section className='container flex flex-col items-center'>
-          <h2 className='text-gray-200 text-center py-4 sm:py-8'>
+          <h2 className='text-blue-200 text-center py-4 sm:py-8'>
             Programing Languages
           </h2>
           <div className='grid grid-cols-2 gap-4'>
             {Languages.map((language) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-gray-400 hover:bg-gray-800 ease-in-out duration-400'
+                className='flex justify-center border border-blue-300 w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-blue-400 hover:bg-gray-800 ease-in-out duration-400 shadow-lg shadow-blue-500/50'
                 key={language.id}
               >
                 <div className='relative group-hover:opacity-100'>
                   <Flip delay={300} duration={2000} left cascade>
                     <div className='text-3xl md:text-5xl'>{language.icon}</div>
                   </Flip>
-                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-gray-200 text-sm font-semibold whitespace-nowrap'>
+                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-blue-300 text-sm font-semibold whitespace-nowrap'>
                     {language.name}
                   </span>
                 </div>
@@ -42,20 +42,20 @@ function Skills() {
         </section>
 
         <section className='container flex flex-col items-center'>
-          <h2 className='text-gray-200 text-center py-4 sm:py-8'>
+          <h2 className='text-blue-200 text-center py-4 sm:py-8'>
             Libraries & Frameworks
           </h2>
           <div className='grid grid-cols-2 gap-4'>
             {Libraries.map((library) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-gray-400 hover:bg-gray-800 ease-in-out duration-400'
+                className='flex justify-center border border-blue-300 w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-blue-400 hover:bg-gray-800 ease-in-out duration-400 shadow-lg shadow-blue-500/50'
                 key={library.id}
               >
                 <div className='relative group-hover:opacity-100'>
                   <Flip delay={300} duration={2000} left cascade>
                     <div className='text-3xl md:text-5xl'>{library.icon}</div>
                   </Flip>
-                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-gray-200 text-sm font-semibold whitespace-nowrap'>
+                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-blue-300 text-sm font-semibold whitespace-nowrap'>
                     {library.name}
                   </span>
                 </div>
@@ -65,14 +65,14 @@ function Skills() {
           <div className='grid grid-cols-2 gap-4 pt-4'>
             {Frameworks.map((framework) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-gray-400 hover:bg-gray-800 ease-in-out duration-400'
+                className='flex justify-center border border-blue-300 w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-blue-400 hover:bg-gray-800 ease-in-out duration-400 shadow-lg shadow-blue-500/50'
                 key={framework.id}
               >
                 <div className='relative group-hover:opacity-100'>
                   <Flip delay={300} duration={2000} left cascade>
                     <div className='text-3xl md:text-5xl'>{framework.icon}</div>
                   </Flip>
-                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-gray-200 text-sm font-semibold whitespace-nowrap'>
+                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-blue-300 text-sm font-semibold whitespace-nowrap'>
                     {framework.name}
                   </span>
                 </div>
@@ -82,20 +82,20 @@ function Skills() {
         </section>
 
         <section className='container flex flex-col items-center'>
-          <h2 className='text-gray-200 text-center py-4 sm:py-8'>
+          <h2 className='text-blue-200 text-center py-4 sm:py-8'>
             Tools and Software
           </h2>
           <div className='grid grid-cols-2 gap-4'>
             {Tools.map((tool) => (
               <div
-                className='flex justify-center border w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-gray-400 hover:bg-gray-800 ease-in-out duration-400'
+                className='flex justify-center border border-blue-300 w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-blue-400 hover:bg-gray-800 transition ease-in-out duration-400 shadow-lg shadow-blue-500/50'
                 key={tool.id}
               >
                 <div className='relative group-hover:opacity-100'>
                   <Flip delay={300} duration={2000} left cascade>
                     <div className='text-3xl md:text-5xl'>{tool.icon}</div>
                   </Flip>
-                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-gray-200 text-sm font-semibold whitespace-nowrap'>
+                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-blue-300 text-sm font-semibold whitespace-nowrap'>
                     {tool.name}
                   </span>
                 </div>
