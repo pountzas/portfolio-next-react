6feacba53148efadc9acdd16726d7c318b01316a
pountzas <nikos@pountzas.gr>
2022-02-11 22:02:02 +0200

    add apollo and graphql settings & client query w/ help of github graphql api

--- pages/Projects.js
+++ pages/Projects.js
@@ -1,5 +1,13 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  gql,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 function Projects() {
   return (
     <div className='bg-gray-700 min-h-screen'>
@@ -8,6 +16,82 @@ function Projects() {
       <Footer />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const httpLink = createHttpLink({
+    uri: 'https://api.github.com/graphql',
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      {
+        user(login: "pountzas") {
+          id
+          pinnedItems(first: 6) {
+            edges {
+              node {
+                ... on Repository {
+                  id
+                  name
+                  forkCount
+                  stargazerCount
+                  openGraphImageUrl
+                  assignableUsers(first: 5) {
+                    edges {
+                      node {
+                        id
+                        avatarUrl
+                        name
+                      }
+                    }
+                  }
+                  description
+                  url
+                  repositoryTopics(first: 6) {
+                    edges {
+                      node {
+                        id
+                        topic {
+                          name
+                        }
+                      }
+                    }
+                  }
+                  watchers {
+                    totalCount
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  const { user } = data;
+  const pinnedItems = user.pinnedItems.edges.map(({ node }) => node);
+  // console.log(pinnedItems);
+
+  return {
+    props: {
+    },
+  };
 }
 
 export default Projects;
