9891eaab8e4019d9456c890eafb6f8525fc4bc17
pountzas <nikos@pountzas.gr>
2022-02-12 00:45:10 +0200

    add Link to github repo on repo cards

--- pages/Projects.js
+++ pages/Projects.js
@@ -10,36 +10,36 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import { AiOutlineStar, AiOutlineFork } from 'react-icons/ai';
+import Link from 'next/link';
 
-function Projects({ pinnedItems }) {
+function Projects({ pinnedItems, contributors, repoTags }) {
   return (
     <div className='bg-gray-700 min-h-screen'>
       <Header />
       <section className='flex justify-center'>
         <div className='grid md:grid-cols-2 gap-8 m-4 p-3 pb-10'>
           {pinnedItems.map((item) => (
-            <div
-              key={item.id}
-              className='max-w-md border-2 border-blue-200 text-blue-200 bg-gray-800 rounded-lg p-4'
-            >
-              <h1 className='text-center text-xl font-semibold p-2'>
-                {item.name}
-              </h1>
-              <Image
-                className='rounded-lg p-4'
-                src={item.openGraphImageUrl}
-                width='640px'
-                height='480px'
-                alt={item.name}
-              />
-              <p className='py-2'>{item.description}</p>
-              <p className='flex justify-center items-center space-x-4 whitespace-normal'>
-                <AiOutlineFork />
-                {item.forkCount}
-                <AiOutlineStar />
-                {item.stargazerCount}
-              </p>
-            </div>
+            <Link href={item.url} key={item.id} passHref>
+              <div className='max-w-md border-2 border-blue-200 text-blue-200 bg-gray-900 rounded-lg p-4'>
+                <h1 className='text-center text-xl font-semibold p-2'>
+                  {item.name}
+                </h1>
+                <Image
+                  className='rounded-lg p-4'
+                  src={item.openGraphImageUrl}
+                  width='640px'
+                  height='480px'
+                  alt={item.name}
+                />
+                <p className='py-2'>{item.description}</p>
+                <div className='flex justify-center items-center space-x-4 whitespace-normal'>
+                  <AiOutlineFork />
+                  {item.forkCount}
+                  <AiOutlineStar />
+                  {item.stargazerCount}
+                </div>
+              </div>
+            </Link>
           ))}
         </div>
       </section>
@@ -116,11 +116,13 @@ export async function getStaticProps() {
 
   const { user } = data;
   const pinned = user.pinnedItems.edges.map(({ node }) => node);
-  // console.log(pinnedItems);
+
+  console.log(pinned);
 
   return {
     props: {
       pinnedItems: pinned,
+      // repoTags: tags,
     },
   };
 }
