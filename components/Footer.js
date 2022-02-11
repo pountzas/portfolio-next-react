edaed4c821e479a520400514140374224decdecb
pountzas <nikos@pountzas.gr>
2022-02-11 13:34:54 +0200

    change text and border color on Footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className='bg-gray-900 bottom-0 min-w-full fixed border-t'>
-      <div className='md:max-w-4xl xl:max-w-6xl mx-auto text-gray-200 flex justify-between items-center'>
+    <div className='bg-gray-900 bottom-0 min-w-full fixed border-t border-blue-200'>
+      <div className='md:max-w-4xl xl:max-w-6xl mx-auto text-blue-200 flex justify-between items-center'>
         <div>Designed and Developed by Pountzas Nikos</div>
         <div>Copyright © 2022 NP</div>
         <div className='flex items-center justify-center space-x-2 py-2'>
