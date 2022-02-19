915da6260a818a7895e4da92f5e1c226940b65e3
pountzas <nikos@pountzas.gr>
2022-02-19 18:16:15 +0200

    apply skillTemplate comp to Libraries skills

--- pages/Skills.js
+++ pages/Skills.js
@@ -41,19 +41,11 @@ function Skills() {
           </h2>
           <div className='grid grid-cols-2 gap-4'>
             {Libraries.map((library) => (
-              <div
-                className='flex justify-center bg-gray-900 border border-blue-300 hover:border-blue-900 w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-blue-400 hover:bg-gray-800 ease-in-out duration-400 shadow-lg shadow-blue-500/50 hover:shadow-blue-900/50'
+              <SkillTemplate
                 key={library.id}
-              >
-                <div className='relative group-hover:opacity-100'>
-                  <Flip duration={2000} left cascade>
-                    <div className='text-3xl md:text-5xl'>{library.icon}</div>
-                  </Flip>
-                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-blue-300 text-sm font-semibold whitespace-nowrap'>
-                    {library.name}
-                  </span>
-                </div>
-              </div>
+                icon={library.icon}
+                skillName={library.name}
+              />
             ))}
           </div>
           <div className='grid grid-cols-2 gap-4 pt-4'>
