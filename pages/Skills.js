03c3e4d4bd0b13043b3f2483ab403cd40e982954
pountzas <nikos@pountzas.gr>
2022-02-19 18:14:46 +0200

    apply skillTemplate comp to Language skills

--- pages/Skills.js
+++ pages/Skills.js
@@ -8,6 +8,7 @@ import Tools from '../components/Tools';
 
 import Flip from 'react-reveal/Flip';
 import config from 'react-reveal/globals';
+import SkillTemplate from '../components/SkillTemplate';
 
 config({ ssrFadeout: true });
 
@@ -25,19 +26,11 @@ function Skills() {
           </h2>
           <div className='grid grid-cols-2 gap-4'>
             {Languages.map((language) => (
-              <div
-                className='flex justify-center bg-gray-900 border border-blue-300 hover:border-blue-900 w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-blue-400 hover:bg-gray-800 ease-in-out duration-400 shadow-lg shadow-blue-500/50 hover:shadow-blue-900/50'
+              <SkillTemplate
                 key={language.id}
-              >
-                <div className='relative group-hover:opacity-100'>
-                  <Flip duration={2000} left cascade>
-                    <div className='text-3xl md:text-5xl'>{language.icon}</div>
-                  </Flip>
-                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-blue-300 text-sm font-semibold whitespace-nowrap'>
-                    {language.name}
-                  </span>
-                </div>
-              </div>
+                icon={language.icon}
+                skillName={language.name}
+              />
             ))}
           </div>
         </section>
