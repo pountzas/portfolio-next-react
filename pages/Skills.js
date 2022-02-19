3a70a830b5c96b65f67e9bf70a80b1811ec1281b
pountzas <nikos@pountzas.gr>
2022-02-19 18:17:36 +0200

    apply skillTemplate comp to Tools skills

--- pages/Skills.js
+++ pages/Skills.js
@@ -61,23 +61,15 @@ function Skills() {
 
         <section className='container flex flex-col items-center'>
           <h2 className='text-blue-200 text-center py-4 sm:py-8'>
-            Tools and Software
+            Tools and Services
           </h2>
           <div className='grid grid-cols-2 gap-4'>
             {Tools.map((tool) => (
-              <div
-                className='flex justify-center bg-gray-900 border border-blue-300 hover:border-blue-900 w-16 h-16 md:w-20 md:h-20 items-center m-2 p-2 rounded-md text-gray-500 hover:text-blue-400 hover:bg-gray-800 ease-in-out duration-400 shadow-lg shadow-blue-500/50 hover:shadow-blue-900/50'
+              <SkillTemplate
                 key={tool.id}
-              >
-                <div className='relative group-hover:opacity-100'>
-                  <Flip duration={2000} left cascade>
-                    <div className='text-3xl md:text-5xl'>{tool.icon}</div>
-                  </Flip>
-                  <span className='absolute inset-0 z-10 -top-10 opacity-0 hover:opacity-100 flex justify-center text-blue-300 text-sm font-semibold whitespace-nowrap'>
-                    {tool.name}
-                  </span>
-                </div>
-              </div>
+                icon={tool.icon}
+                skillName={tool.name}
+              />
             ))}
           </div>
         </section>
