3a382b02bb1d44e1d05c95ae37ab9606186dc539
pountzas <nikos@pountzas.gr>
2022-10-21 14:46:08 +0300

    change footer colors to new pallete

--- components/Footer.js
+++ components/Footer.js
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className='bg-gray-900 bottom-0 min-w-full fixed border-t border-blue-200'>
-      <div className='md:max-w-4xl xl:max-w-6xl mx-auto text-blue-200 flex justify-between items-center'>
+    <div className='fixed bottom-0 min-w-full border-t border-borderSecondary bg-primary'>
+      <div className='flex items-center justify-between mx-auto text-textTertiary md:max-w-4xl xl:max-w-6xl'>
         <div className='hidden md:block'>
           Designed and Developed by Pountzas Nikos
         </div>
         <div>Copyright © 2022 NP</div>
-        <div className='flex items-center justify-center space-x-2 py-2'>
+        <div className='flex items-center justify-center py-2 space-x-2'>
           {Socials.map((social) => (
             <div key={social.id}>
               <Link href={social.path} passHref>
