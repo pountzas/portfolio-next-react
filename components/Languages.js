54b82704f47e2f0f33df667c512eb0da0652578a
pountzas <nikos@pountzas.gr>
2022-10-23 00:40:22 +0300

    adjust Languages props

--- components/Languages.js
+++ components/Languages.js
@@ -7,22 +7,22 @@ const Languages = [
   {
     id: 1,
     name: 'HTML5',
-    icon: <Html5 />,
+    icon: <Html5 fill='#9C98B0' />,
   },
   {
     id: 2,
     name: 'CSS3',
-    icon: <Css3 />,
+    icon: <Css3 fill='#9C98B0' />,
   },
   {
     id: 3,
     name: 'Ruby',
-    icon: <Ruby />,
+    icon: <Ruby fill='#9C98B0' />,
   },
   {
     id: 4,
     name: 'JavaScript',
-    icon: <Javascript />,
+    icon: <Javascript fill='#373D42' fill2='#9C98B0' />,
   },
   // {
   //   id: 5,
